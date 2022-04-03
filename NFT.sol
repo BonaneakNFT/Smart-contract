15446f3f0925e54f0588ae0ebe1966f2da4567b1
BonaneakNFT <101963458+BonaneakNFT@users.noreply.github.com>
2022-04-03 09:51:21 +0700

    Update NFT.sol

--- NFT.sol
+++ NFT.sol
@@ -15,47 +15,50 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFT is ERC721Enumerable, Ownable {
+contract SimpleNftLowerGas is ERC721, Ownable {
   using Strings for uint256;
+  using Counters for Counters.Counter;
 
-  string public baseURI;
-  string public baseExtension = ".json";
-  uint256 public cost = 0.005 ether;
+  Counters.Counter private supply;
+
+  string public uriPrefix = "";
+  string public uriSuffix = ".json";
+  string public hiddenMetadataUri;
+  
+  uint256 public cost = 0.01 ether;
   uint256 public maxSupply = 10000;
-  uint256 public maxMintAmount = 100;
-  bool public paused = false;
-  mapping(address => bool) public whitelisted;
+  uint256 public maxMintAmountPerTx = 5;
 
-  constructor() ERC721("NFT Collection", "NFT") {
-    setBaseURI("https://cloud.genfty.com/ipfs/QmPkzbWtHYShADfa2QC8y1kWjY3y1vJgzWdMz2uUgwLbyg/");
-    mint(msg.sender, 100);
+  bool public paused = true;
+  bool public revealed = false;
+
+  constructor() ERC721("NAME", "SYMBOL") {
+    setHiddenMetadataUri("ipfs://__CID__/hidden.json");
   }
 
-  // internal
-  function _baseURI() internal view virtual override returns (string memory) {
-    return baseURI;
+  modifier mintCompliance(uint256 _mintAmount) {
+    require(_mintAmount > 0 && _mintAmount <= maxMintAmountPerTx, "Invalid mint amount!");
+    require(supply.current() + _mintAmount <= maxSupply, "Max supply exceeded!");
+    _;
   }
 
-  // public
-  function mint(address _to, uint256 _mintAmount) public payable {
-    uint256 supply = totalSupply();
-    require(!paused);
-    require(_mintAmount > 0);
-    require(_mintAmount <= maxMintAmount);
-    require(supply + _mintAmount <= maxSupply);
+  function totalSupply() public view returns (uint256) {
+    return supply.current();
+  }
 
-    if (msg.sender != owner()) {
-        if(whitelisted[msg.sender] != true) {
-          require(msg.value >= cost * _mintAmount);
-        }
-    }
+  function mint(uint256 _mintAmount) public payable mintCompliance(_mintAmount) {
+    require(!paused, "The contract is paused!");
+    require(msg.value >= cost * _mintAmount, "Insufficient funds!");
 
-    for (uint256 i = 1; i <= _mintAmount; i++) {
-      _safeMint(_to, supply + i);
-    }
+    _mintLoop(msg.sender, _mintAmount);
+  }
+  
+  function mintForAddress(uint256 _mintAmount, address _receiver) public mintCompliance(_mintAmount) onlyOwner {
+    _mintLoop(_receiver, _mintAmount);
   }
 
   function walletOfOwner(address _owner)
@@ -64,14 +67,26 @@ contract NFT is ERC721Enumerable, Ownable {
     returns (uint256[] memory)
   {
     uint256 ownerTokenCount = balanceOf(_owner);
-    uint256[] memory tokenIds = new uint256[](ownerTokenCount);
-    for (uint256 i; i < ownerTokenCount; i++) {
-      tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+    uint256[] memory ownedTokenIds = new uint256[](ownerTokenCount);
+    uint256 currentTokenId = 1;
+    uint256 ownedTokenIndex = 0;
+
+    while (ownedTokenIndex < ownerTokenCount && currentTokenId <= maxSupply) {
+      address currentTokenOwner = ownerOf(currentTokenId);
+
+      if (currentTokenOwner == _owner) {
+        ownedTokenIds[ownedTokenIndex] = currentTokenId;
+
+        ownedTokenIndex++;
+      }
+
+      currentTokenId++;
     }
-    return tokenIds;
+
+    return ownedTokenIds;
   }
 
-  function tokenURI(uint256 tokenId)
+  function tokenURI(uint256 _tokenId)
     public
     view
     virtual
@@ -79,58 +94,72 @@ contract NFT is ERC721Enumerable, Ownable {
     returns (string memory)
   {
     require(
-      _exists(tokenId),
+      _exists(_tokenId),
       "ERC721Metadata: URI query for nonexistent token"
     );
 
+    if (revealed == false) {
+      return hiddenMetadataUri;
+    }
+
     string memory currentBaseURI = _baseURI();
     return bytes(currentBaseURI).length > 0
-        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
+        ? string(abi.encodePacked(currentBaseURI, _tokenId.toString(), uriSuffix))
         : "";
   }
 
-  //only owner
-  function setCost(uint256 _newCost) public onlyOwner {
-    cost = _newCost;
+  function setRevealed(bool _state) public onlyOwner {
+    revealed = _state;
   }
 
-  function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
-    maxMintAmount = _newmaxMintAmount;
+  function setCost(uint256 _cost) public onlyOwner {
+    cost = _cost;
   }
 
-  function setBaseURI(string memory _newBaseURI) public onlyOwner {
-    baseURI = _newBaseURI;
+  function setMaxMintAmountPerTx(uint256 _maxMintAmountPerTx) public onlyOwner {
+    maxMintAmountPerTx = _maxMintAmountPerTx;
   }
 
-  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
-    baseExtension = _newBaseExtension;
+  function setHiddenMetadataUri(string memory _hiddenMetadataUri) public onlyOwner {
+    hiddenMetadataUri = _hiddenMetadataUri;
   }
 
-  function pause(bool _state) public onlyOwner {
+  function setUriPrefix(string memory _uriPrefix) public onlyOwner {
+    uriPrefix = _uriPrefix;
+  }
+
+  function setUriSuffix(string memory _uriSuffix) public onlyOwner {
+    uriSuffix = _uriSuffix;
+  }
+
+  function setPaused(bool _state) public onlyOwner {
     paused = _state;
   }
- 
-  function whitelistUser(address _user) public onlyOwner {
-    whitelisted[_user] = true;
-  }
- 
-  function removeWhitelistUser(address _user) public onlyOwner {
-    whitelisted[_user] = false;
-  }
 
-  function withdraw() public payable onlyOwner {
-    // This will pay Genfty.com 5% of the initial sale.
-    // You can remove this if you want, or keep it in to support Genfty.com.
+  function withdraw() public onlyOwner {
+    // This will pay HashLips 5% of the initial sale.
+    // You can remove this if you want, or keep it in to support HashLips and his channel.
     // =============================================================================
     (bool hs, ) = payable(0xF58D90984b6b75E7AF782Db6D91Df00176b5A00f).call{value: address(this).balance * 5 / 100}("");
     require(hs);
     // =============================================================================
-    
-    // This will payout the owner 95% of the contract balance.
+
+    // This will transfer the remaining contract balance to the owner.
     // Do not remove this otherwise you will not be able to withdraw the funds.
     // =============================================================================
     (bool os, ) = payable(owner()).call{value: address(this).balance}("");
     require(os);
     // =============================================================================
+  }
+
+  function _mintLoop(address _receiver, uint256 _mintAmount) internal {
+    for (uint256 i = 0; i < _mintAmount; i++) {
+      supply.increment();
+      _safeMint(_receiver, supply.current());
+    }
+  }
+
+  function _baseURI() internal view virtual override returns (string memory) {
+    return uriPrefix;
   }
 }
